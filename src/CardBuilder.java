829d918447243dd4f3724c560e685b47f1da5ab2
Adam Nixon <88755070+DAdamNixon@users.noreply.github.com>
2022-12-01 16:57:32 -0600

    card formatting added

--- src/CardBuilder.java
+++ src/CardBuilder.java
@@ -1,7 +1,11 @@
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.util.Builder;
 
 public class CardBuilder implements Builder<Region> {
@@ -15,12 +19,13 @@ public class CardBuilder implements Builder<Region> {
     @Override
     public Region build() {
         VBox card = new VBox();
-        card.setPadding(new Insets(3));
+        card.setPadding(new Insets(15));
         card.setMinHeight(180);
         card.setMinWidth(180);
         card.getChildren().addAll(
                 new Label(model.getItemName()),
                 new Label(String.format("%.2f", model.getPrice())));
+        card.setBackground(new Background(new BackgroundFill(Color.WHITE , new CornerRadii(10), new Insets(10))));
         return card;
     }
 
