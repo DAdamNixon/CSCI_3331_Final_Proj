99eec2b8b56bddc35dc5026760232210cd1ccfa3
HonestPhoenix <113068231+Zburgess1@users.noreply.github.com>
2022-12-03 11:58:57 -0600

    Update LoginPage.java

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -16,7 +16,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 
 public class LoginPage extends BorderPane {
 
@@ -25,26 +24,15 @@ public class LoginPage extends BorderPane {
 	private TextField password;
 	private Button btnLogin;
 	private Button btnQuit;
-	private Label lblTitle;
-	private HBox titleBox;
 
 	LoginPage(Store main) {
-		this.setBackground(new Background(new BackgroundFill(Color.LAVENDER, CornerRadii.EMPTY, Insets.EMPTY)));
-		this.lblTitle = new Label("The MarketPlace");
-		this.lblTitle.setFont(new Font("Consolas", 40));
-		this.titleBox = new HBox(lblTitle);
-		this.titleBox.setPadding(new Insets(20));
 		this.mainStore = main;
 		this.username = new TextField();
 		this.password = new TextField();
 		this.btnLogin = new Button("Login");
 		this.btnQuit = new Button("Quit");
-		this.setBackground(new Background(new BackgroundImage(new Image(Resources.imagePath("login.png")), null, null, null, null)));
-		IconView icon = new IconView(Resources.imagePath("flour.png"));
-		this.titleBox.getChildren().add(icon);
-		//this.setTop(titleBox);
-		this.titleBox.setAlignment(Pos.CENTER);
-
+		this.setBackground(new Background(
+				new BackgroundImage(new Image(Resources.imagePath("login.png")), null, null, null, null)));
 		btnLogin.setOnAction(e -> {
 			this.mainStore.loginAttempt(username.getText(), password.getText());
 		});
@@ -61,7 +49,8 @@ public class LoginPage extends BorderPane {
 		Region spacer = new Region();
 		spacer.setPrefHeight(140);
 		GridPane gp = new GridPane();
-		container.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(10), new Insets(260, 100, 100, 100))));
+		container.setBackground(
+				new Background(new BackgroundFill(Color.WHITE, new CornerRadii(10), new Insets(260, 100, 100, 100))));
 		GridPane.setMargin(gp, new Insets(80, 0, 0, 0));
 		HBox buttonBox = new HBox();
 		buttonBox.getChildren().addAll(btnLogin, btnQuit);
