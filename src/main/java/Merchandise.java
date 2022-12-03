fb0416ab342d7aea1a3a41bf4e6dee3c2e08c43f
HonestPhoenix <113068231+Zburgess1@users.noreply.github.com>
2022-12-03 13:32:27 -0600

    Commented Merchandise

--- src/main/java/Merchandise.java
+++ src/main/java/Merchandise.java
@@ -1,36 +1,40 @@
 package main.java;
 
+// Abtract class Merchandise which Automotive, Grocery, and Meat Items inherit from 
 public abstract class Merchandise {
 
+    // Shared Fields
     protected int itemNumber;
-
     protected float price;
-
     protected int inStock;
-
     protected String brandName;
-
     protected String itemName;
-
     protected boolean taxable;
 
+    // Filepath for card image
     protected String filePath;
 
+    // Returns the name of the item
     public final String getItemName() {
         return this.itemName;
     }
 
+    // Returns the price of the item
     public final double getPrice() {
         return this.price;
     }
 
+    // Returns the image filepath
     public final String getpath() {
         return this.filePath;
     }
 
+    // Returns whether the item is taxable or not
     public boolean taxable() {
         return this.taxable;
     }
+
+    // Returns a string representation of important attributes
     @Override
     public String toString() {
         return this.getItemName() + "," + this.getPrice();
